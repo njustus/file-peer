1d184fd529cee7f35687db18da65571ccdff3081
N. Justus <n-justus@outlook.com>
2020-11-24 19:24:55 +0100

    stream incoming files into download directory

--- src/main/scala/filepeer/core/FilePeerMain.scala
+++ src/main/scala/filepeer/core/FilePeerMain.scala
@@ -1,5 +1,7 @@
 package filepeer.core
 
+import java.nio.file.Paths
+
 import akka.actor.ActorSystem
 import akka.io.Udp
 import akka.stream.Materializer
@@ -12,8 +14,10 @@ import pureconfig._
 import pureconfig.generic.auto._
 
 object FilePeerMain {
+
   def main(args: Array[String]): Unit = {
     implicit val env = ConfigSource.default.at("file-peer").loadOrThrow[Env]
+    TransferService.createTargetDir(env.transfer)
 
     implicit val system = ActorSystem("file-peer")
     implicit val mat = Materializer.matFromSystem
--- src/main/scala/filepeer/core/transfer/TransferService.scala
+++ src/main/scala/filepeer/core/transfer/TransferService.scala
@@ -1,6 +1,8 @@
 package filepeer.core.transfer
 
-import java.nio.file.Path
+import java.nio.file.{Files, Path}
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 import akka.NotUsed
 import akka.actor.ActorSystem
@@ -8,8 +10,8 @@ import akka.stream.Materializer
 import akka.stream.scaladsl._
 import akka.util.ByteString
 import com.typesafe.scalalogging.LazyLogging
-import filepeer.core.Env
-import filepeer.core.transfer.TransferService.{TransferMsg, TransferPreview}
+import filepeer.core.{Env, TransferEnv}
+import filepeer.core.transfer.TransferService.{FileTransfer, TransferMsg, TransferPreview}
 import io.circe._
 import io.circe.generic.JsonCodec
 import io.circe.generic.auto._
@@ -19,7 +21,8 @@ import io.circe.syntax._
 import scala.util.{Failure, Success}
 
 class TransferService()(implicit actorSystem: ActorSystem, mat: Materializer, env: Env) extends LazyLogging {
-  Tcp().bind(env.transfer.address.host, env.transfer.address.port).runForeach { connection =>
+  val transferEnv = env.transfer
+  Tcp().bind(transferEnv.address.host, transferEnv.address.port).runForeach { connection =>
     val flow = readJson
       .via(handlingFlow)
       .via(writeJson)
@@ -38,6 +41,31 @@ class TransferService()(implicit actorSystem: ActorSystem, mat: Materializer, en
 
   def writeJson[T: Encoder]: Flow[T, ByteString, NotUsed] = Flow[T].map { x => ByteString(x.asJson.noSpaces) }
 
+  def fileSource(path: Path) = FileIO.fromPath(path)
+    .fold(ByteString.empty) { (acc, bs) => acc ++ bs }
+    .map { bs => FileTransfer(path.getFileName.toString, bs.encodeBase64.toArray) }
+
+  def fileSink = {
+    Flow[FileTransfer].map { case FileTransfer(fileName, content) =>
+        val path = this.targetPath(fileName)
+        logger.info(s"saving $fileName at $path")
+        val bs = ByteString(content).decodeBase64
+        (path, bs)
+    }
+      .mapAsyncUnordered(2) { case (path, bs) => Source.single(bs).runWith(FileIO.toPath(path)) }
+      .to(Sink.ignore)
+  }
+
+  def targetPath(fileName: String): Path = {
+    val timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+    val targetFile = transferEnv.targetDir.resolve(fileName)
+    if(!Files.exists(targetFile)) {
+      targetFile
+    } else {
+      transferEnv.targetDir.resolve(timestamp+"-"+fileName)
+    }
+  }
+
   def handlingFlow = {
     Flow[TransferMsg].map { x =>
       logger.debug(s"received: $x")
@@ -50,4 +78,9 @@ object TransferService {
   @JsonCodec
   sealed trait TransferMsg
   case class TransferPreview(fileNames: List[String]) extends TransferMsg
+  case class FileTransfer(fileName: String, content: Array[Byte]) extends TransferMsg
+
+  def createTargetDir(conf: TransferEnv): Unit = {
+    better.files.File(conf.targetDir).createDirectories()
+  }
 }
