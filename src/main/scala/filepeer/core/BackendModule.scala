80dc2367fca1b17d57f76a278d1136c1e8006dcd
N. Justus <n-justus@outlook.com>
2020-12-24 11:25:04 +0100

    add BackendModule for bootstrapping backend services

---
+++ src/main/scala/filepeer/core/BackendModule.scala
@@ -0,0 +1,23 @@
+package filepeer.core
+
+import akka.actor.ActorSystem
+import akka.stream.Materializer
+import filepeer.core.discovery.DiscoveryService
+import filepeer.core.discovery.DiscoveryService.DiscoverySubscriber
+import filepeer.core.transfer.FileReceiver.{FileSaved, FileSavedObserver}
+import filepeer.core.transfer.{FileReceiver, TransferServer}
+import pureconfig.ConfigSource
+
+class BackendModule(discoverySubscriber: DiscoverySubscriber, receiverSubscriber: FileSavedObserver)(implicit env: Env) {
+    TransferServer.createTargetDir(env.transfer)
+
+    implicit val system = ActorSystem("file-peer")
+    implicit val mat = Materializer.matFromSystem
+    scala.sys.addShutdownHook {
+      system.terminate()
+    }
+
+  val fileReceiver = new FileReceiver(receiverSubscriber)
+  val discoveryService = new DiscoveryService(discoverySubscriber)
+  val transferServer = new TransferServer(fileReceiver)
+}
