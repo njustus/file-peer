f90b05cd3874d1fa4b75ead0f03de1a9d55b877a
N. Justus <n-justus@outlook.com>
2020-12-24 10:46:29 +0100

    load fxml main view

--- src/main/scala/filepeer/ui/FilePeerUi.scala
+++ src/main/scala/filepeer/ui/FilePeerUi.scala
@@ -1,23 +1,20 @@
 package filepeer.ui
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.application.Application
+import javafx.event.ActionEvent
+import javafx.event.EventHandler
+import javafx.fxml.FXMLLoader
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.layout.{StackPane, VBox}
 import javafx.stage.Stage;
 
 class FilePeerUi extends Application {
   override def start(primaryStage:Stage):Unit = {
-    primaryStage.setTitle("Hello World!");
-    val btn = new Button();
-    btn.setText("Say 'Hello World'");
-    btn.setOnAction(event => println("Hello World!"))
+    primaryStage.setTitle("FilePeer")
 
-    val root = new StackPane();
-    root.getChildren().add(btn);
-    primaryStage.setScene(new Scene(root, 300, 250));
+    val mainView:VBox = FXMLLoader.load(getClass.getResource("/views/main-view.fxml"))
+    primaryStage.setScene(new Scene(mainView, 600, 400));
     primaryStage.show();
   }
 }
