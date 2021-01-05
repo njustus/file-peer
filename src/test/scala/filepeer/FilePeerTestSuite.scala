07e7e47c373a89621488f66850710f654e71eea6
N. Justus <n-justus@outlook.com>
2021-01-05 16:00:36 +0100

    create a separate FilePeerTestSuite for tests without a required ActorSystem

---
+++ src/test/scala/filepeer/FilePeerTestSuite.scala
@@ -0,0 +1,17 @@
+package filepeer
+
+import filepeer.core.Env
+import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, Inspectors}
+import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.matchers.should
+import pureconfig._
+import pureconfig.generic.auto._
+
+trait FilePeerTestSuite
+  extends AsyncFlatSpec
+    with should.Matchers
+    with Inspectors
+    with BeforeAndAfterAll
+    with BeforeAndAfter {
+  implicit val env = ConfigSource.default.at("file-peer").loadOrThrow[Env]
+}
--- src/test/scala/filepeer/core/ActorTestSuite.scala
+++ src/test/scala/filepeer/core/ActorTestSuite.scala
@@ -2,15 +2,11 @@ package filepeer.core
 
 import akka.actor.ActorSystem
 import akka.stream.Materializer
-import org.scalatest._
-import matchers._
-import flatspec._
-import scala.concurrent.duration._
-import pureconfig._
-import pureconfig.generic.auto._
+import filepeer.FilePeerTestSuite
 
-abstract class ActorTestSuite extends AsyncFlatSpec with should.Matchers with Inspectors with BeforeAndAfterAll {
-  implicit val env = ConfigSource.default.at("file-peer").loadOrThrow[Env]
+import scala.concurrent.duration._
+
+abstract class ActorTestSuite extends FilePeerTestSuite {
   implicit val system = ActorSystem("filepeer-testsystem")
   implicit val mat = Materializer.matFromSystem
   implicit val exec = system.dispatcher
