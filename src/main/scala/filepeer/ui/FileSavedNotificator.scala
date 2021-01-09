6a39f5f5f4600e3a336376350fdc3e787e11ba81
N. Justus <n-justus@outlook.com>
2021-01-09 18:38:48 +0100

    move notifications into 'FileSavedNotificator'

---
+++ src/main/scala/filepeer/ui/FileSavedNotificator.scala
@@ -0,0 +1,65 @@
+package filepeer.ui
+
+import java.awt.Desktop
+import java.net.URL
+import java.nio.file.Files
+import java.util.ResourceBundle
+
+import com.typesafe.scalalogging.LazyLogging
+import filepeer.core.Env
+import filepeer.core.transfer.FileReceiver
+import javafx.application.Platform
+import javafx.util.Duration
+import org.controlsfx.control.Notifications
+import org.controlsfx.control.action.Action
+
+import scala.concurrent.{ExecutionContext, Future, blocking}
+import javafx.event.ActionEvent
+import javafx.stage.Window
+
+trait FileSavedNotificator {
+  self: LazyLogging =>
+
+  protected def notifyFileSaved(fileSaved: FileReceiver.FileSaved): Unit = {
+    logger.info(s"File received: $fileSaved")
+    val openDownloadDirectoryAction = new Action("Open Directory", (_:ActionEvent) => this.openDownloadDirectory())
+    val openFileAction = new Action("Open File", (_:ActionEvent) => this.openFile(fileSaved))
+
+    val size = Files.size(fileSaved.path).toDouble / 1024 //TODO use message header instead of recalculation
+
+    Platform.runLater { () =>
+      Notifications.create()
+        .owner(currentWindow)
+        .title("File received.")
+        .hideAfter(Duration.seconds(5))
+        .action(openFileAction, openDownloadDirectoryAction)
+        .text(f"Received: ${fileSaved.name} ($size%.2f KB)")
+        .showInformation()
+    }
+  }
+
+  private def openDownloadDirectory(): Unit = {
+    withDesktop(_.open(env.downloadDir.toFile))
+  }
+
+  private def openFile(fs: FileReceiver.FileSaved): Unit = {
+    withDesktop(_.open(fs.path.toFile))
+  }
+
+  private def withDesktop[A](fn: Desktop => A): Future[A] = {
+    if(Desktop.isDesktopSupported) {
+      //must run in separate thread to not block the whole application
+      Future {
+        blocking {
+          fn(Desktop.getDesktop)
+        }
+      }
+    } else {
+      throw new UnsupportedOperationException("Unsupported Desktop Environment! Can not open default application.")
+    }
+  }
+
+  protected def env: Env
+  protected def currentWindow: Window
+  protected implicit def blockingExecutor: ExecutionContext
+}
--- src/main/scala/filepeer/ui/FileSendingController.scala
+++ src/main/scala/filepeer/ui/FileSendingController.scala
@@ -9,26 +9,27 @@ import cats.data.NonEmptyList
 import com.typesafe.scalalogging.LazyLogging
 import filepeer.core.Env
 import filepeer.core.discovery.DiscoveryService.ClientName
-import filepeer.core.transfer.{Client, FileReceiver}
+import filepeer.core.transfer.Client
 import filepeer.ui.state.{UiState, UiStateController}
 import javafx.application.Platform
-import javafx.event.ActionEvent
 import javafx.fxml.{FXML, Initializable}
 import javafx.scene.control.Label
 import javafx.scene.input.{DragEvent, TransferMode}
 import javafx.scene.layout.HBox
 import javafx.scene.layout.StackPane
 import javafx.scene.layout.VBox
-import javafx.util.Duration
-import org.controlsfx.control.Notifications
-import org.controlsfx.control.action.Action
+import javafx.stage.Window
 import rx.lang.scala.Subscription
 import rx.lang.scala.subscriptions.CompositeSubscription
 
-import scala.concurrent.ExecutionContext.Implicits._
-import scala.concurrent.{Future, blocking}
+import scala.concurrent.ExecutionContext
 
-class FileSendingController(env:Env, fileSender: Client) extends LazyLogging with Initializable with UiStateController  {
+class FileSendingController(override val env:Env,
+                            fileSender: Client)
+  extends FileSavedNotificator
+    with LazyLogging
+    with Initializable
+    with UiStateController  {
 
   import scala.language.implicitConversions
   import scala.jdk.CollectionConverters._
@@ -92,42 +93,6 @@ class FileSendingController(env:Env, fileSender: Client) extends LazyLogging wit
     CompositeSubscription(clientSub, fsSub)
   }
 
-  private def notifyFileSaved(fileSaved: FileReceiver.FileSaved): Unit = {
-    logger.info(s"File received: $fileSaved")
-    val openDownloadDirectoryAction = new Action("Open Directory", (_:ActionEvent) => this.openDownloadDirectory())
-    val openFileAction = new Action("Open File", (_:ActionEvent) => this.openFile(fileSaved))
-
-    val size = Files.size(fileSaved.path).toDouble / 1024 //TODO use message header instead of recalculation
-
-    Platform.runLater { () =>
-      Notifications.create()
-        .owner(stackPane.getScene.getWindow)
-        .title("File received.")
-        .hideAfter(Duration.seconds(5))
-        .action(openFileAction, openDownloadDirectoryAction)
-        .text(f"Received: ${fileSaved.name} ($size%.2f KB)")
-        .showInformation()
-    }
-  }
-
-  private def openDownloadDirectory(): Unit = {
-    withDesktop(_.open(env.downloadDir.toFile))
-  }
-
-  private def openFile(fs: FileReceiver.FileSaved): Unit = {
-    withDesktop(_.open(fs.path.toFile))
-  }
-
-  private def withDesktop[A](fn: Desktop => A): Future[A] = {
-    if(Desktop.isDesktopSupported) {
-      //must run in separate thread to not block the whole application
-      Future {
-        blocking {
-          fn(Desktop.getDesktop)
-        }
-      }
-    } else {
-      throw new UnsupportedOperationException("Unsupported Desktop Environment! Can not open default application.")
-    }
-  }
+  override protected def currentWindow: Window = stackPane.getScene.getWindow
+  override protected implicit def blockingExecutor: ExecutionContext = ExecutionContext.global
 }
