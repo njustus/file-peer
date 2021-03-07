e682827abf5a0e94535d91282331affce090df5b
N. Justus <n-justus@outlook.com>
2021-03-07 12:14:24 +0100

    extend ClientNameSpec

--- src/test/scala/filepeer/core/discovery/ClientNameSpec.scala
+++ src/test/scala/filepeer/core/discovery/ClientNameSpec.scala
@@ -3,6 +3,7 @@ package filepeer.core.discovery
 import filepeer.FilePeerTestSuite
 import filepeer.core._
 import scala.collection.mutable
+import java.time.Instant
 
 class ClientNameSpec extends FilePeerTestSuite {
   import DiscoveryService.ClientName
@@ -17,12 +18,17 @@ class ClientNameSpec extends FilePeerTestSuite {
     (cl == cl) shouldBe (true)
   }
 
-  it should "implement equals based on ip" in {
-    val cl2 = cl.copy(hostName="nico")
+  it should "implement equals based on ip returning 'true'" in {
+    val cl2 = cl.copy(hostName="nico", discoveredAt=Instant.now)
     (cl == cl2) shouldBe (true)
 
     val cl3 = cl.copy(hostName="nico", port=7001)
     (cl == cl3) shouldBe (true)
+  }
+
+  it should "implement equals based on ip returning 'false'" in {
+    val cl2 = cl.copy(ip="127.1.0.0")
+    (cl == cl2) shouldBe (false)
   }
 
   it should "be useable inside a LinkedHashSet" in {
@@ -31,9 +37,14 @@ class ClientNameSpec extends FilePeerTestSuite {
     hashSet.size shouldBe (1)
 
     hashSet += cl.copy()
-    hashSet.size shouldBe (1)
+    hashSet should have size(1)
 
     hashSet += ClientName("nico", "192.168.0.1", 7080)
-    hashSet.size shouldBe (2)
+    hashSet should have size(2)
+
+    println(s"hashset: $hashSet")
+
+    hashSet should contain (cl.copy(hostName="test", discoveredAt=Instant.now))
+    hashSet should not contain (cl.copy(ip="127.1.0.01"))
   }
 }
