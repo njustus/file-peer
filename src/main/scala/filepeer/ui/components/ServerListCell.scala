e705cbd0f440cbc43e6aaaa3d493aee1d4fdb8e6
N. Justus <n-justus@outlook.com>
2021-05-16 18:14:34 +0200

    display laptop icons in ListCell #5

--- src/main/scala/filepeer/ui/components/ServerListCell.scala
+++ src/main/scala/filepeer/ui/components/ServerListCell.scala
@@ -2,18 +2,24 @@ package filepeer.ui.components
 
 import filepeer.core.discovery.DiscoveryService
 import javafx.scene.control.{Label, ListCell}
-import javafx.scene.layout.VBox
+import javafx.scene.layout.{HBox, VBox}
+import org.controlsfx.glyphfont.{FontAwesome, Glyph}
 
 private[components] class ServerListCell() extends ListCell[DiscoveryService.ClientName] {
 
-  private val rootNode = new VBox()
+  private val rootNode = new HBox()
+
+  private val textRootNode = new VBox()
   private val nameLbl = new Label()
   private val addressLbl = new Label()
+  private val icon = new Glyph("FontAwesome", FontAwesome.Glyph.LAPTOP)
 
+  rootNode.getStyleClass.add("serverlist-cell-wrapper")
   nameLbl.getStyleClass.addAll("serverlist-cell", "serverlist-cell-name")
   addressLbl.getStyleClass.addAll("serverlist-cell", "serverlist-cell-address")
 
-  rootNode.getChildren.addAll(nameLbl, addressLbl)
+  rootNode.getChildren.addAll(icon, textRootNode)
+  textRootNode.getChildren.addAll(nameLbl, addressLbl)
 
   override protected def updateItem(clientName: DiscoveryService.ClientName, empty: Boolean): Unit = {
     super.updateItem(clientName, empty)
@@ -26,6 +32,7 @@ private[components] class ServerListCell() extends ListCell[DiscoveryService.Cli
 
       if(clientName.isLocalhost) {
         this.getStyleClass().add("serverlist-cell--localhost")
+        icon.setIcon(FontAwesome.Glyph.HOME)
       }
 
       setGraphic(rootNode)
